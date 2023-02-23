2698c6d52e4fa33fe489e0faeeec16b804c9fd70
yassineDr12 <drafate19@gmail.com>
2023-02-23 02:52:13 +0100

    f

--- SurFill/settings.py
+++ SurFill/settings.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from pathlib import Path
 import django_heroku
 import dj_database_url
@@ -7,7 +7,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(verbose=True, dotenv_path=os.path.join(BASE_DIR, '.env'))
+#load_dotenv(verbose=True, dotenv_path=os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
