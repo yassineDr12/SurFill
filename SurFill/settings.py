659246a773621f99a5c638fbdcebfa45f4d2d60a
Yassine Drafate <yassinedrafate1@gmail.com>
2023-03-05 16:45:48 +0100

    Latest working version

--- SurFill/settings.py
+++ SurFill/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
