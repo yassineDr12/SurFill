254f341666a92bb40f2ca2d4e8d0a7d7b994c61a
Yassine Drafate <yassinedrafate1@gmail.com>
2023-03-07 03:06:29 +0100

    staticfiles fox

--- SurFill/settings.py
+++ SurFill/settings.py
@@ -151,7 +151,6 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
